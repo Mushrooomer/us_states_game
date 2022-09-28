fcb1c8c66559f0f1393aada4bf8f982ded74e57b
krystofklempir <k.klempir@gmail.com>
2022-09-28 21:39:06 +0000

    United States Quiz done

--- main.py
+++ main.py
@@ -14,8 +14,16 @@ guessed_states = []
 while len(guessed_states) < 50:
     answer_state = screen.textinput(title=f"{len(guessed_states)}/50 States Correct",
                                     prompt="What is another state's name?").title()
-    print(answer_state)
 
+    if answer_state == "Exit":
+        missing_states = []
+        for state in all_states:
+            if state not in guessed_states:
+                missing_states.append(state)
+        new_data = pandas.DataFrame(missing_states)
+        new_data.to_csv("states_to_learn.csv")
+
+        break
     if answer_state in all_states:
         guessed_states.append(answer_state)
         t = turtle.Turtle()
@@ -25,10 +33,4 @@ while len(guessed_states) < 50:
         t.goto(int(state_data.x), int(state_data.y))
         t.write(answer_state)
 
-
-
-
-
-
-
-screen.exitonclick()
+# States to learn.csv
